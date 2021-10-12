6dc1f06344839f1fa50f1f9e5391300bb882bad9
ravenkiller1709 <kim@kimkruse.xyz>
2021-10-12 11:57:01 +0200

    adding dotfiles from rep

---
+++ .emacs.d/var/nsm-settings.el
@@ -0,0 +1,3 @@
+(
+ (:id "sha1:697e0c1494e7bd0acdd06fc4cb9346682a0941f9" :fingerprints ("sha1:3e:86:d0:e9:5f:ef:07:56:bf:e2:d8:1f:19:36:47:ea:55:6c:71:37") :conditions nil)
+)
---
+++ .emacs.d/var/tramp/persistency.el
@@ -0,0 +1,10 @@
+;; -*- emacs-lisp -*- <21/10/11 08:51:12 /home/kim/.emacs.d/var/tramp/persistency.el>
+;; Tramp connection history.  Don't change this file.
+;; Run `M-x tramp-cleanup-all-connections' instead.
+
+(((tramp-file-name "sudoedit" "root" nil "Arch" nil nil nil)
+  ("uid-integer" 0)
+  ("gid-integer" 0)
+  ("uid-string" "root")
+  ("gid-string" "root")
+  ("case-insensitive" nil)))
